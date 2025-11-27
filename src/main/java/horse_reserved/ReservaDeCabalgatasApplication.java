010a3d73c7d7c5c05c9eaefd015666321d9c50b5
Adriaann Mauricio Rodriguez Rubiano <149967949+azrrael22@users.noreply.github.com>
2025-11-27 17:07:31 -0500

    Primer commmit

---
+++ src/main/java/horse_reserved/ReservaDeCabalgatasApplication.java
@@ -0,0 +1,13 @@
+package horse_reserved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaDeCabalgatasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaDeCabalgatasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/horse_reserved/ReservaDeCabalgatasApplicationTests.java
@@ -0,0 +1,13 @@
+package horse_reserved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaDeCabalgatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
